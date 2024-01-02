3378499f73d14d49b637de529c450c79bb4eae5c
Utkarsh Pandey <utkarshpandey911@gmail.com>
2024-01-03 00:07:21 +0530

    Initial commit

---
+++ home/terminal.nix
@@ -0,0 +1,69 @@
+{ pkgs, ... }:
+
+# Platform-independent terminal setup
+{
+  # Nix packages to install to $HOME
+  #
+  # Search for packages here: https://search.nixos.org/packages
+  home.packages = with pkgs; [
+    # Unix tools
+    ripgrep # Better `grep`
+    fd
+    sd
+    tree
+
+    # Nix dev
+    cachix
+    nil # Nix language server
+    nix-info
+    nixpkgs-fmt
+    nixci
+
+    # Dev
+    just
+    lazygit # Better git UI
+    tmate
+  ];
+
+  home.shellAliases = rec {
+    g = "git";
+    lg = "lazygit";
+  };
+
+  # Programs natively supported by home-manager.
+  programs = {
+    bat.enable = true;
+    # Type `z <pat>` to cd to some directory
+    zoxide.enable = true;
+    # Type `<ctrl> + r` to fuzzy search your shell history
+    fzf.enable = true;
+    jq.enable = true;
+    nix-index.enable = true;
+    htop.enable = true;
+
+
+    # on macOS, you probably don't need this
+    bash = {
+      enable = true;
+      initExtra = ''
+        # Make Nix and home-manager installed things available in PATH.
+        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+      '';
+    };
+
+    # For macOS's default shell.
+    zsh = {
+      enable = true;
+      envExtra = ''
+        # Make Nix and home-manager installed things available in PATH.
+        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+      '';
+    };
+
+    # https://zero-to-flakes.com/direnv
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
+}
